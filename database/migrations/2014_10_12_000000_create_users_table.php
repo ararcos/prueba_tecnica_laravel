bdb021cb4756d15f87f53d9489ef375494caf15d
Alexander <alexanderarcos66@gmail.com>
2021-04-23 12:23:19 -0500

    Proyecto Base

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('nombre',100);
+            $table->string('numero',10)->nullable();
+            $table->string('cedula',11);
+            $table->date('fecha_nacimiento');
+            $table->string('ciudad',100);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //limpia la tabla de usuarios primero
+        User::truncate();
+        //encripta la contraseña admin
+        $password = Hash::make('admin');
+        //crea el usuario admin
+        User::create([
+            'email'=> 'admin@admin.com',
+            'password'=>$password,
+            'nombre'=>'Admin',
+            'numero'=>'0987654321',
+            'cedula'=>'18765432123',
+            'fecha_nacimiento'=>'1999-05-14',
+            'ciudad'=>'Ambato',
+        ]);
+
+        //Correr seeder comando : php artisan db:seed --class=UsersTableSeeder
+    }
+}
